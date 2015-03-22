71615e885abf19300ab95bbce73512a783a0e29e
doracl <wangchunlei1980@gmail.com>
2015-03-22 20:35:57 +0800

    test session

--- controllers/default.go
+++ controllers/default.go
@@ -1,17 +1,34 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/session"
 	"github.com/doracl/gocrm/auth"
 	"github.com/doracl/gocrm/core"
 )
+
+var globalSession *session.Manager
+
+func init() {
+	return
+	fmt.Println("start session")
+	globalSession, _ := session.NewManager("memory", `{"cookieName":"gosessionid", "enableSetCookie,omitempty": true, "gclifetime":3600, "maxLifetime": 3600, "secure": false, "sessionIDHashFunc": "sha1", "sessionIDHashKey": "", "cookieLifeTime": 3600, "providerConfig": ""}`)
+	go globalSession.GC()
+}
 
 type MainController struct {
 	beego.Controller
 }
 
 func (c *MainController) Get() {
-	c.Data["IsLogin"] = false
+	islogin := false
+	user := c.Ctx.Input.CruSession.Get("user")
+	if user != nil {
+		islogin = true
+	}
+	c.Data["IsLogin"] = islogin
+	c.Data["UserName"] = user
 	c.Data["Name"] = "github"
 	c.Data["NameLower"] = "github"
 	c.TplNames = "index.tpl"
@@ -31,6 +48,10 @@ func (c *MainController) Oauth() {
 	token := github.GetToken(code)
 
 	user := github.GetUserInfo(token)
+
+	sess := c.Ctx.Input.CruSession
+
+	sess.Set("user", user)
 
 	c.Data["IsLogin"] = true
 	c.Data["UserName"] = user
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+
 	"github.com/doracl/gocrm/auth"
 	"github.com/doracl/gocrm/core"
 	_ "github.com/doracl/gocrm/routers"
@@ -11,6 +12,9 @@ func initialize() {
 	beego.AppConfig.Set("github_appid", "06d12a51a03abd64895c")
 	beego.AppConfig.Set("github_appsecret", "6412d9d1b103bb7809c58ec0ae9cf1245559695a")
 	beego.AppConfig.Set("baseurl", "http://192.168.70.179:8081")
+
+	beego.SessionOn = true
+	beego.SessionProvider = "memory"
 
 	core.App = make(map[string]interface{})
 	core.App["authprovider"] = auth.NewGithub()
